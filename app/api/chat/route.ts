8c75a1e2480449e9fb04786606770fc9151bb7a0
VladSolyankin <darnell.dewberry@mail.ru>
2024-04-23 13:46:17 +0300

    Изменения

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,24 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
 import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
-// but configure it to point to fireworks.ai
-const fireworks = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_FIREWORKS_TOKEN || "",
-  baseURL: "https://api.fireworks.ai/inference/v1",
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAPI_KEY,
 });
-// IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
+
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  // Ask Fireworks for a streaming chat completion using Llama 2 70b model
-  // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
-  const response = await fireworks.chat.completions.create({
-    model: "accounts/fireworks/models/llama-v2-70b-chat",
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
     stream: true,
-    max_tokens: 1000,
     messages,
   });
-  // Convert the response into a friendly text-stream.
+
+  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
